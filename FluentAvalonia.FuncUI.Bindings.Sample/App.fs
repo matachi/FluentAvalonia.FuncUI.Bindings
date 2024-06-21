d1101fc7c94c7bb80c367ddb2d90d67dc613c4a5
Daniel Jonsson <daniel.jonsson@kalyna.se>
2024-06-21 22:50:03 +0200

    Add sample project

---
+++ FluentAvalonia.FuncUI.Bindings.Sample/App.fs
@@ -0,0 +1,51 @@
+namespace FluentAvalonia.FuncUI.Bindings.Sample
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.FuncUI.Hosts
+open Avalonia.Controls
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open FluentAvalonia.FuncUI.Bindings
+open FluentAvalonia.Styling
+open FluentAvalonia.UI.Controls
+
+module Main =
+    let navigationViewItems =
+        [ NavigationViewItem(Content = "Test 1")
+          NavigationViewItem(Content = "Test 2")
+          NavigationViewItem(Content = "Test 3") ]
+
+    let saveSelectedNavigationViewItem (selectedNavigationViewItem: IWritable<NavigationViewItem>) (newlySelectedNavigationViewItem: obj) =
+        match newlySelectedNavigationViewItem with
+        | :? NavigationViewItem as newItem -> selectedNavigationViewItem.Set newItem
+        | _ -> ()
+
+    let view =
+        Component(fun ctx ->
+            let selectedNavigationViewItem = ctx.useState navigationViewItems[0]
+
+            NavigationView.create
+                [ NavigationView.menuItemsSource navigationViewItems
+                  NavigationView.selectedItem selectedNavigationViewItem.Current
+                  NavigationView.onSelectedItemChanged (saveSelectedNavigationViewItem selectedNavigationViewItem)
+                  NavigationView.content (TextBlock.create [ TextBlock.text (selectedNavigationViewItem.Current.Content :?> string) ]) ])
+
+type MainWindow() =
+    inherit HostWindow()
+
+    do
+        base.Title <- "Sample app"
+        base.Content <- Main.view
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() = this.Styles.Add(FluentAvaloniaTheme())
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime -> desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+        base.OnFrameworkInitializationCompleted()
---
+++ FluentAvalonia.FuncUI.Bindings.Sample/Program.fs
@@ -0,0 +1,20 @@
+namespace MasterUkrainian.Desktop
+open System
+open Avalonia
+open Avalonia.ReactiveUI
+open FluentAvalonia.FuncUI.Bindings.Sample
+
+module Program =
+
+    [<CompiledName "BuildAvaloniaApp">] 
+    let buildAvaloniaApp () = 
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace(areas = Array.empty)
+            .UseReactiveUI()
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
